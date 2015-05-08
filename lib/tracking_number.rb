5468c0fb9198a5a25dc38d487f5d334a4468425b
Jeff Keen <jeff@keen.me>
2015-05-07 19:07:42 -0500

    Split DHL class into DHLExpress and DHLExpressAir, to support both types of numbers

--- lib/tracking_number.rb
+++ lib/tracking_number.rb
@@ -15,7 +15,7 @@ if defined?(ActiveModel::EachValidator)
 end
 
 module TrackingNumber
-  TYPES = [UPS, FedExExpress, FedExSmartPost, FedExGround, FedExGround18, FedExGround96, USPS91, USPS20, USPS13, DHL, OnTrac]
+  TYPES = [UPS, FedExExpress, FedExSmartPost, FedExGround, FedExGround18, FedExGround96, USPS91, USPS20, USPS13, DHLExpress, DHLExpressAir, OnTrac]
 
   def self.search(body)
     TYPES.collect { |type| type.search(body) }.flatten
--- test/dhl_tracking_number_test.rb
+++ test/dhl_tracking_number_test.rb
@@ -1,14 +1,26 @@
 require 'test_helper'
 
 class DHLTrackingNumberTest < Minitest::Test
-  context "a DHL tracking number" do
+  context "DHLExpressAir tracking number" do
     ["73891051146"].each do |valid_number|
       should "return dhl for #{valid_number}" do
-        should_be_valid_number(valid_number, TrackingNumber::DHL, :dhl)
+        should_be_valid_number(valid_number, TrackingNumber::DHLExpressAir, :dhl)
       end
 
       should "detect #{valid_number} regardless of spacing" do
-        should_detect_number_variants(valid_number, TrackingNumber::DHL)
+        should_detect_number_variants(valid_number, TrackingNumber::DHLExpressAir)
+      end
+    end
+  end
+
+  context "DHLExpress tracking numbers" do
+    ["3318810025", "8487135506", "3318810036", "3318810014"].each do |valid_number|
+      should "return dhl for #{valid_number}" do
+        should_be_valid_number(valid_number, TrackingNumber::DHLExpress, :dhl)
+      end
+
+      should "detect #{valid_number} regardless of spacing" do
+        should_detect_number_variants(valid_number, TrackingNumber::DHLExpress)
       end
     end
   end
