6c72e289e7e1ff4f34727f4d3ef2667f22ed68eb
Jeff Keen <jeff@keen.me>
2022-11-05 16:44:37 -0500

    Add mod 37/36 checksum validation

--- lib/tracking_number/checksum_validations.rb
+++ lib/tracking_number/checksum_validations.rb
@@ -60,6 +60,32 @@ module TrackingNumber
         # standard mod 7 check
         return true if sequence.to_i % 7 == check_digit.to_i
       end
+
+      def validates_mod_37_36?(sequence, check_digit, extras = {})
+        # From https://esolutions.dpd.com/dokumente/DPD_Parcel_Label_Specification_2.4.1_EN.pdf
+
+        mod = 36
+        weights = {A: 10, B: 11, C: 12, D: 13, E: 14, F: 15, G: 16, H: 17, I: 18, J: 19, K: 20, L: 21, M: 22, N: 23, O: 24, P: 25, Q: 26, R: 27, S: 28, T: 29, U: 30, V: 31, W: 32, X: 33, Y: 34, Z: 35}
+        cd = mod
+        sequence.chars.to_a.each do |char, i|
+          val = (char =~ /[A-Za-z]/  ? weights[char.to_sym] : char.to_i)
+
+          cd = val + cd
+          cd = cd - mod if cd > mod
+          cd = cd * 2
+          cd = cd - (mod + 1) if cd > mod
+        end
+
+        cd = (mod + 1) - cd
+        cd = 0 if cd == mod
+        computed = if cd >= 10
+                     weights.find { |a, val| val == cd }[0].to_s
+                   else
+                     cd.to_s
+                   end
+
+        computed == check_digit
+      end
     end
   end
 end
