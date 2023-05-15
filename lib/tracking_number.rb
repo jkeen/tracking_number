75fe5304e933679aff89a91a93d2b7cf58c1fe5c
Jeff Keen <jeff@keen.me>
2023-05-15 14:16:19 -0500

    feat: add partnership support with tracking_number_data 1.5
    chore: run some much needed rubocops to clean/format things

--- lib/tracking_number.rb
+++ lib/tracking_number.rb
@@ -5,6 +5,7 @@ require 'tracking_number/checksum_validations'
 require 'tracking_number/loader'
 require 'tracking_number/base'
 require 'tracking_number/info'
+require 'tracking_number/partnership'
 require 'tracking_number/unknown'
 require 'active_support/core_ext/string'
 require 'active_support/core_ext/hash'
@@ -16,29 +17,51 @@ end
 TrackingNumber::Loader.load_tracking_number_data
 
 module TrackingNumber
-  def self.search(body)
-    TYPES.collect { |type| type.search(body) }.flatten
+  def self.search(body, match: :carrier)
+    matches = TYPES.collect { |type| type.search(body) }.flatten
+
+    # Some tracking numbers (e.g. Fedex Smartpost) are partnerships between two parties, where one party is the shipper (e.g. Fedex)
+    # and the other party is the [last mile] carrier (e.g. USPS). We're probably interested in the last mile aspect of
+    # the partnership, so by default we'll show those
+
+    # Tracking numbers without a partnership are both the shipper and carrier.
+
+    case match
+    when :carrier
+      matches.filter(&:carrier?)
+    when :shipper
+      matches.filter(&:shipper?)
+    when :all
+      matches
+    else
+      matches
+    end
   end
 
-  def self.detect(tracking_number)
+  def self.detect(tracking_number, match: :carrier)
     tn = nil
-    for test_klass in (TYPES+[Unknown])
+    (TYPES + [Unknown]).each do |test_klass|
       tn = test_klass.new(tracking_number)
-      break if tn.valid?
+      if tn.valid? && (!match || match == :all)
+        break
+      elsif tn.valid? && tn.send("#{match}?")
+        break
+      end
     end
-    return tn
+    tn
   end
 
   def self.detect_all(tracking_number)
     matches = []
-    for test_klass in (TYPES+[Unknown])
+    (TYPES + [Unknown]).each do |test_klass|
       tn = test_klass.new(tracking_number)
       matches << tn if tn.valid?
     end
-    return matches
+
+    matches
   end
 
   def self.new(tracking_number)
-    self.detect(tracking_number)
+    detect(tracking_number)
   end
 end
--- lib/tracking_number/loader.rb
+++ lib/tracking_number/loader.rb
@@ -1,22 +1,21 @@
-
 module TrackingNumber
   module Loader
     class << self
-      def load_tracking_number_data(couriers_path = File.join(File.dirname(__FILE__), "../data/couriers/"))
+      def load_tracking_number_data(couriers_path = File.join(File.dirname(__FILE__), '../data/couriers/'))
         mapping_data = []
         tracking_number_types = []
         Dir.glob(File.join(couriers_path, '/*.json')).each do |file|
-          courier_info          = read_courier_info(file)
+          courier_info = read_courier_info(file)
 
           courier_info[:tracking_numbers].each do |tracking_info|
-            tracking_name = tracking_info[:name]
-            klass         = create_class(klass, courier_info, tracking_info)
+            klass = create_class(courier_info, tracking_info)
 
             # Do some basic checks on the data file
             throw 'missing test numbers' unless has_test_numbers?(tracking_info)
-            throw 'missing regex match groups' unless test_numbers_return_required_groups?(tracking_info, Regexp.new(klass::VERIFY_PATTERN))
+            throw 'missing regex match groups' unless test_numbers_return_required_groups?(tracking_info,
+                                                                                           Regexp.new(klass::VERIFY_PATTERN))
+            const = register_class(klass, tracking_info[:name])
 
-            const = register_class(klass, tracking_name)
             tracking_number_types.push(const)
             mapping_data << {
               class: const,
@@ -28,33 +27,33 @@ module TrackingNumber
         end
 
         TrackingNumber.const_set('TYPES', tracking_number_types)
-        
-        return mapping_data
+
+        mapping_data
       end
 
       private
 
       def has_test_numbers?(tracking)
-        return tracking[:test_numbers] && tracking[:test_numbers][:valid]
+        tracking[:test_numbers] && tracking[:test_numbers][:valid]
       end
 
       def test_numbers_return_required_groups?(tracking, regex)
         test_number = tracking[:test_numbers][:valid][0]
         matches = test_number.match(regex)
 
-        return matches["SerialNumber"]
+        matches['SerialNumber']
       end
 
       def read_courier_info(file)
-        return JSON.parse(File.read(file)).deep_symbolize_keys!
+        JSON.parse(File.read(file)).deep_symbolize_keys!
       end
 
-      def create_class(klass, courier_info, tracking_info)
+      def create_class(courier_info, tracking_info)
         klass = Class.new(TrackingNumber::Base)
-        klass.const_set("COURIER_CODE", courier_info[:courier_code])
+        klass.const_set('COURIER_CODE', courier_info[:courier_code])
         info = courier_info.dup
         info.delete(:tracking_numbers)
-        klass.const_set("COURIER_INFO", info)
+        klass.const_set('COURIER_INFO', info)
 
         pattern = tracking_info[:regex]
         pattern = tracking_info[:regex].join if tracking_info[:regex].is_a?(Array)
@@ -62,19 +61,21 @@ module TrackingNumber
         verify_pattern = "^#{pattern}$"
         search_pattern = "\\b#{pattern}\\b"
 
-        klass.const_set("SEARCH_PATTERN", Regexp.new(search_pattern))
-        klass.const_set("VERIFY_PATTERN", Regexp.new(verify_pattern))
+        klass.const_set('SEARCH_PATTERN', Regexp.new(search_pattern))
+        klass.const_set('VERIFY_PATTERN', Regexp.new(verify_pattern))
 
-        klass.const_set("VALIDATION", tracking_info[:validation])
-        klass.const_set("ADDITIONAL", tracking_info[:additional])
-        klass.const_set("TRACKING_URL", tracking_info[:tracking_url])
+        klass.const_set('VALIDATION', tracking_info[:validation])
+        klass.const_set('ADDITIONAL', tracking_info[:additional])
+        klass.const_set('TRACKING_URL', tracking_info[:tracking_url])
+        klass.const_set('PARTNERS', tracking_info[:partners])
+        klass.const_set('ID', tracking_info[:id])
 
-        return klass
+        klass
       end
 
       def register_class(klass, tracking_name)
         klass_name = tracking_name.gsub(/[^0-9A-Za-z]/, '')
-        return TrackingNumber.const_set(klass_name, klass)
+        TrackingNumber.const_set(klass_name, klass)
       end
     end
   end
